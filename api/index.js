999ce4a2e9095caa608ed6e9f088c113b9bbfd23
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-03-29 18:01:27 -0500

    gestion avanzada de errores throw

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const config = require('../config.js')
 const user = require('./components/user/network')
 const auth = require('./components/auth/network')
+const errors = require('../network/errors')
 
 const app = express()
 
@@ -12,6 +13,8 @@ app.use(bodyParser.json())
 // ROUER
 app.use('/api/user', user)
 app.use('/api/auth', auth)
+
+app.use(errors)
 
 app.listen(config.api.port, () => {
   console.log('Api listen in port ', config.api.port)
