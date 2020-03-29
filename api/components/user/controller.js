73d29280119f3c7f886e406a36556051a6ca10ce
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-03-29 00:09:24 -0500

    Rutas para usuarios

--- api/components/user/controller.js
+++ api/components/user/controller.js
@@ -1,11 +1,40 @@
-const store = require('../../../store/dummy')
-
 const TABLA = 'user'
 
-function list() {
-  return store.list(TABLA)
+module.exports = function (injectedStore) {
+  let store = injectedStore
+  if (!store) {
+    store = require('../../../store/dummy')
+  }
+  function list() {
+    return store.list(TABLA)
+  }
+  
+  function get(id) {
+    return store.get(TABLA, id)
+  }
+
+  return {
+    list,
+    get
+  }
 }
 
-module.exports = {
-  list
-}
+// module.exports = (injectedStore) => {
+//   if(!injectedStore) injectedStore = require("../../../store/dummy");
+
+//   return {
+//     list: () => injectedStore.list(TABLE),
+//     get: id => injectedStore.get(TABLE, id)
+//   };
+// };
+
+// module.exports = (injectedStore = require('../../store/dummy')) => {
+
+//   return {
+//     list: () => injectedStore.list(TABLE),
+//     get: (id) => injectedStore.get(TABLE, id),
+//     upsert: (payload = null) => injectedStore.get(TABLE, payload),
+//     _delete: (id) => injectedStore.get(TABLE, id)
+
+//   }
+// }
