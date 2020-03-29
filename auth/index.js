265980f8157d50c46d6313543247bf4d27a6fdfc
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-03-29 17:36:44 -0500

    verificacion con token

--- auth/index.js
+++ auth/index.js
@@ -1,45 +1,51 @@
-const jwt = require('jsonwebtoken')
-const config = require('../config')
-const secret = config.jwt.secret
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const secret = config.jwt.secret;
 
 function sign(data) {
-  return jwt.sign(data, secret)
+    return jwt.sign(data, secret);
 }
 
 function verify(token) {
-  return jwt.verify(token, secret)
+    return jwt.verify(token, secret)
 }
 
 const check = {
-  own: function(req, owner) {
-    const decoded = decodeHeader(req)
-    console.log(decoded)
-  }
+    own: function(req, owner) {
+        const decoded = decodeHeader(req);
+        console.log(decoded);
+
+        if (decoded.id !== owner) {
+            throw new Error('No puedes hacer esto');
+        }
+    },
 }
 
 function getToken(auth) {
-  if (!auth) {
-    throw new Error('no token comes')
-  }
+    if (!auth) {
+        throw new Error('No viene token');
+    }
 
-  if (auth.indexOf('Bearer ') === -1) {
-    throw new Error('Invalid format')
-  }
+    if (auth.indexOf('Bearer ') === -1) {
+        throw new Error('Formato invalido');
+    }
 
-  let token = auth.replace('Bearer', '')
-  return token
+    let token = auth.replace('Bearer ', '');
+    return token;
 }
 
 function decodeHeader(req) {
-  const authorization = req.headers.authorization || ''
-  const token = getToken(authorization)
-  const decoded = verify(token)
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    const decoded = verify(token);
 
-  req.user = decoded
+    req.user = decoded;
 
-  return decoded
+    return decoded;
 }
 
 module.exports = {
-  sign
-}
+    sign,
+    check,
+};
