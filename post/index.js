1b32966c6cace46489c0795bf6ff68c53818eda4
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-03-30 12:33:06 -0500

    separando los posts a un microservicio

--- post/index.js
+++ post/index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const config = require('../config.js')
-const user = require('./components/user/network')
-const auth = require('./components/auth/network')
+const post = require('./components/post/network')
 const errors = require('../network/errors')
 
 const app = express()
@@ -11,11 +10,10 @@ const app = express()
 app.use(bodyParser.json())
 
 // ROUER
-app.use('/api/user', user)
-app.use('/api/auth', auth)
+app.use('/api/post', post)
 
 app.use(errors)
 
-app.listen(config.api.port, () => {
-  console.log('Api listen in port ', config.api.port)
+app.listen(config.post.port, () => {
+  console.log('Service post listen in port ', config.post.port)
 })
