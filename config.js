42a6d8fb2c8075d1bbc9907d84bb12ea91d4367d
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-03-29 19:37:41 -0500

    Base de datos real: MySQL

--- config.js
+++ config.js
@@ -4,5 +4,11 @@ module.exports = {
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'notasecret!'
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST || 'remotemysql.com',
+    user: process.env.MYSQL_USER || 'qrOVxOHUnR',
+    password: process.env.MYSQL_PASS || 'gyDB72ePym',
+    database: process.env.MYSQL_DB || 'qrOVxOHUnR',
   }
 }
