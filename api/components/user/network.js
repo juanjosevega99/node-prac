f1cbec22fd03a3ca197667989464b72148206634
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-03-29 22:39:04 -0500

    relacionando entidades follow

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -8,6 +8,7 @@ const router = express.Router()
 
 // ROUTES
 router.get('/', list)
+router.post('/follow/:id', secure('follow'), follow)
 router.get('/:id', get)
 router.post('/', upsert)
 router.put('/', secure('update'), upsert)
@@ -37,5 +38,12 @@ function upsert(req, res, next) {
     .catch(next)
 }
 
+function follow(req, res, next) {
+  controller.follow(req.user.id, req.params.id)
+    .then(data => {
+      response.success(req, res, data, 201)
+    })
+    .catch(next)
+}
 
 module.exports = router
