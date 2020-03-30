bbab947b96376ac8bc4d670c295fdcea3e473f6a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-03-29 22:59:29 -0500

    reto

--- api/components/user/controller.js
+++ api/components/user/controller.js
@@ -45,11 +45,20 @@ module.exports = function (injectedStore) {
     })
   }
 
+  async function following(user) {
+    const join = {}
+    join[TABLA] = 'user_to'
+    const query = {user_from: user}
+
+    return await store.query(TABLA + '_follow', query, join)
+  }
+
   return {
     list,
     get,
     upsert,
-    follow
+    follow,
+    following
   }
 }
 
--- api/components/user/network.js
+++ api/components/user/network.js
@@ -9,6 +9,7 @@ const router = express.Router()
 // ROUTES
 router.get('/', list)
 router.post('/follow/:id', secure('follow'), follow)
+router.get('/:id/following', following)
 router.get('/:id', get)
 router.post('/', upsert)
 router.put('/', secure('update'), upsert)
@@ -42,6 +43,14 @@ function follow(req, res, next) {
   controller.follow(req.user.id, req.params.id)
     .then(data => {
       response.success(req, res, data, 201)
+    })
+    .catch(next)
+}
+
+function following(req, res, next) {
+  controller.following(req.params.id)
+    .then(data => {
+      response.success(req, res, data, 200)
     })
     .catch(next)
 }
