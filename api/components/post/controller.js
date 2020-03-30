85f1c9c66d790ace163729beeacd760173085874
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-03-30 10:30:16 -0500

    reto

--- api/components/post/controller.js
+++ api/components/post/controller.js
@@ -1,4 +1,8 @@
 const TABLA = 'post'
+const { nanoid } = require('nanoid');
+const { error } = require('../../../utils/error');
+
+const COLLECTION = 'post'
 
 module.exports = function (injectedStore) {
   let store = injectedStore
@@ -7,10 +11,51 @@ module.exports = function (injectedStore) {
   }
 
   function list() {
-    return store.list(TABLA)
+    return store.list(COLLECTION)
   }
 
+  async function get(id) {
+    const user = await store.get(COLLECTION, id)
+    if (!user) {
+      throw error('The post doesnt exist', 400)
+    }
+
+    return user
+  }
+
+  async function upsert(data, user) {
+    const post = {
+      id: data.id,
+      user: user,
+      text: data.text,
+    }
+
+    if (!post.id) {
+      post.id = nanoid()
+    }
+
+    return store.upsert(COLLECTION, post).then(() => post)
+  }
+
+  async function like(post, user) {
+    const like = await store.upsert(COLLECTION + '_like', {
+      post: post,
+      user: user,
+    })
+
+    return like
+  }
+
+  async function postLikers(post) {
+    const users = await store.query(COLLECTION + 'like', { post: post }, {post: post})
+    return users
+  }
   return {
-    list
+    list,
+    get,
+    upsert,
+    like,
+    postsLiked,
+    postLikers
   }
 }
--- api/components/post/network.js
+++ api/components/post/network.js
@@ -1,18 +1,65 @@
 const express = require('express')
 
 const response = require('../../../network/response')
+const auth = require('./secure')
 const controller = require('./index')
 
 const router = express.Router()
 
-// ROUTES
-router.get('/', list)
+// SET ROUTES
+router.get('/', auth('list'), list);
+router.get('/like', auth('list_own'), postsLiked);
+router.get('/:id', auth('get'), get);
+router.post('/', auth('add',), upsert);
+router.put('/', auth('update', { owner: 'user' }), upsert);
+router.post('/:id/like', auth('add'), like);
+router.get('/:id/like', auth('list'), postLikers);
 
-// FUNCTIONS
+// ROUTER FUNCTIONS
 function list(req, res, next) {
   controller.list()
-    .then(data => {
-      response.success(req, res, data, 200)
+    .then(post => {
+      response.success(req, res, post, 200)
+    })
+    .catch(next)
+}
+
+function get(req, res, next) {
+  controller.get(req.params.id)
+    .then(post => {
+      response.success(req, res, post, 200)
+    })
+    .catch(next)
+}
+
+function upsert(req, res, next) {
+  controller.upsert(req.body, req.user.id)
+    .then(post => {
+      response.success(req, res, post, 201)
+    })
+    .catch(next)
+}
+
+function like(req, res, next) {
+  controller.like(req.params.id, req.user.sub)
+    .then(post => {
+      response.success(req, res, post, 201)
+    })
+    .catch(next)
+}
+
+function postLikers(req, res, next) {
+  controller.postLikers(req.params.id)
+    .then(post => {
+      response.success(req, res, post, 200)
+    })
+    .catch(next)
+}
+
+function postsLiked(req, res, next) {
+  controller.postsLiked(req.user.sub)
+    .then(post => {
+      response.success(req, res, post, 200)
     })
     .catch(next)
 }
